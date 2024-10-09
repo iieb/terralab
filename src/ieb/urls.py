7ec23a5d345136b3db98ddbcf14d7463cb53d076
epassarojr <epassarojr@gmail.com>
2024-10-09 17:12:49 -0300

    indicators logic updated

--- src/ieb/urls.py
+++ src/ieb/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import atividade_registro_view, load_componentes, load_atividades, load_equipes, load_equipes_adicionais,load_indicadores, atividade_registro_detalhe_view, teste_parcerias_view,adicionar_parceria, adicionar_plano,atualizar_situacao_plano
+from .views import atividade_registro_view, load_componentes, load_atividades, load_equipes, load_equipes_adicionais,load_indicadores, atividade_registro_detalhe_view, teste_parcerias_view,adicionar_parceria, adicionar_plano,atualizar_situacao_plano, adicionar_produto, adicionar_contrato, atualizar_estado_contrato, adicionar_lei, atualizar_situacao_lei, adicionar_modelo
 
 urlpatterns = [
     path('atividade_registro/', atividade_registro_view, name='atividade_registro'),
@@ -15,6 +15,13 @@ urlpatterns = [
     path('adicionar_parceria/', adicionar_parceria, name='adicionar_parceria'),  # Adicione esta linha
     path('adicionar-plano/', adicionar_plano, name='adicionar_plano'),
     path('atualizar_situacao_plano/', atualizar_situacao_plano, name='atualizar_situacao_plano'),
+    path('adicionar_produto/', adicionar_produto, name='adicionar_produto'),
+    path('adicionar_contrato/', adicionar_contrato, name='adicionar_contrato'),
+    path('atualizar_estado_contrato/', atualizar_estado_contrato, name='atualizar_estado_contrato'),
+    path('adicionar_lei/', adicionar_lei, name='adicionar_lei'),
+    path('atualizar_situacao_lei/', atualizar_situacao_lei, name='atualizar_situacao_lei'),
+    path('adicionar_modelo/', adicionar_modelo, name='adicionar_modelo'),
+
 
 
 ]
